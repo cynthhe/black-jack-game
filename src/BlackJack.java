7501fe934ad0155c53258a839b699e8b8caf6593
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2018-12-26 14:30:59 -0500

    Update BlackJack.java

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,27 +1,3 @@
-/*
- * BlackJack.java
- * Author: Cynthia He
- * Submission Date: December 2, 2018
- *
- * Purpose: This program contains methods relating to the BlackJack class. It represents the
- * actual game of the blackjack being played.
- *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither
- * received nor given inappropriate assistance. I have not copied
- * or modified code from any source other than the course webpage
- * or the course textbook. I recognize that any unauthorized
- * assistance or plagiarism will be handled in accordance with
- * the University of Georgia's Academic Honesty Policy and the
- * policies of this course. I recognize that my work is based
- * on an assignment created by the Department of Computer
- * Science at the University of Georgia. Any publishing
- * or posting of source code for this project is strictly
- * prohibited unless you have written consent from the Department
- * of Computer Science at the University of Georgia.
- */
-
 /**
  * Class representing a single player blackjack game
  */
